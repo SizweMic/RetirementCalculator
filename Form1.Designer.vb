ea2bb510de75242fe5142de1f9b0bac61afc52e3
malum <malum@LAPTOP-OBQ6GA6K>
2022-08-03 16:08:17 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,217 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtInterestRate = New System.Windows.Forms.TextBox()
+        Me.txtMonthlyPayment = New System.Windows.Forms.TextBox()
+        Me.txtAge = New System.Windows.Forms.TextBox()
+        Me.cmbRetirementAge = New System.Windows.Forms.ComboBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblDisplay = New System.Windows.Forms.Label()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.txtInterestRate)
+        Me.GroupBox1.Controls.Add(Me.txtMonthlyPayment)
+        Me.GroupBox1.Controls.Add(Me.txtAge)
+        Me.GroupBox1.Controls.Add(Me.cmbRetirementAge)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.GroupBox1.Location = New System.Drawing.Point(62, 76)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(349, 150)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Calculate the future value of your investment: "
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(215, 61)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(49, 15)
+        Me.Label6.TabIndex = 9
+        Me.Label6.Text = "in years"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label5.Location = New System.Drawing.Point(215, 123)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(88, 15)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "% (per annum)"
+        '
+        'txtInterestRate
+        '
+        Me.txtInterestRate.Location = New System.Drawing.Point(151, 115)
+        Me.txtInterestRate.Name = "txtInterestRate"
+        Me.txtInterestRate.Size = New System.Drawing.Size(58, 23)
+        Me.txtInterestRate.TabIndex = 7
+        '
+        'txtMonthlyPayment
+        '
+        Me.txtMonthlyPayment.Location = New System.Drawing.Point(150, 87)
+        Me.txtMonthlyPayment.Name = "txtMonthlyPayment"
+        Me.txtMonthlyPayment.Size = New System.Drawing.Size(121, 23)
+        Me.txtMonthlyPayment.TabIndex = 6
+        '
+        'txtAge
+        '
+        Me.txtAge.Location = New System.Drawing.Point(150, 58)
+        Me.txtAge.Name = "txtAge"
+        Me.txtAge.Size = New System.Drawing.Size(59, 23)
+        Me.txtAge.TabIndex = 5
+        '
+        'cmbRetirementAge
+        '
+        Me.cmbRetirementAge.FormattingEnabled = True
+        Me.cmbRetirementAge.Location = New System.Drawing.Point(150, 29)
+        Me.cmbRetirementAge.Name = "cmbRetirementAge"
+        Me.cmbRetirementAge.Size = New System.Drawing.Size(121, 23)
+        Me.cmbRetirementAge.TabIndex = 4
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(67, 123)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(81, 15)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Interest Rate"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(37, 95)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(105, 15)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Monthly Payment"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(67, 66)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(75, 15)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Current Age"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 37)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(138, 15)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Select Retirement Age:"
+        '
+        'lblDisplay
+        '
+        Me.lblDisplay.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblDisplay.Location = New System.Drawing.Point(62, 242)
+        Me.lblDisplay.Name = "lblDisplay"
+        Me.lblDisplay.Size = New System.Drawing.Size(349, 47)
+        Me.lblDisplay.TabIndex = 1
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(62, 317)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(86, 33)
+        Me.btnCalculate.TabIndex = 2
+        Me.btnCalculate.Text = "Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(175, 317)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(96, 33)
+        Me.btnClear.TabIndex = 3
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(296, 317)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(115, 33)
+        Me.btnExit.TabIndex = 4
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(471, 402)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.lblDisplay)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txtInterestRate As TextBox
+    Friend WithEvents txtMonthlyPayment As TextBox
+    Friend WithEvents txtAge As TextBox
+    Friend WithEvents cmbRetirementAge As ComboBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lblDisplay As Label
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+End Class
---
+++ Form1.vb
@@ -0,0 +1,82 @@
+﻿Public Class Form1
+    '192145476
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        '1.2.1 
+        cmbRetirementAge.Items.Add("55")
+        cmbRetirementAge.Items.Add("60")
+        cmbRetirementAge.Items.Add("65")
+        '1.2.2
+        cmbRetirementAge.SelectedIndex = 0
+        cmbRetirementAge.Focus()
+    End Sub
+    '1.3
+    Public Function CalcPaymentPeriod(ByVal intRetirementAge As Integer, ByVal intCurrentAge As Integer)
+
+        Dim NPer As Integer
+        NPer = (intRetirementAge - intCurrentAge) * 12
+        Return NPer
+
+    End Function
+    '1.5.3
+    Public Function MonthlyInterestRate(ByRef InterestRate)
+
+        Dim Rate As Decimal
+        Rate = InterestRate / 100 / 12
+        Return Rate
+
+    End Function
+
+    Public Function PaymentPerMonth(ByVal MonthlyPayment)
+
+        Dim PMT As Integer
+        PMT = MonthlyPayment
+        Return PMT
+
+    End Function
+    '1.4
+    Public Function CalcFutureValue(ByVal PMT As Integer, ByVal NPer As Integer, ByVal Rate As Decimal)
+
+        Dim FV As Decimal
+        FV = PMT * ((1 + Rate) ^ NPer - 1) / Rate
+
+        Return "The future value is: " & FV.ToString("R###,###,###,##")
+
+    End Function
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+
+        Application.Exit()
+
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+
+        cmbRetirementAge.SelectedIndex = 0
+        cmbRetirementAge.Focus()
+        txtAge.Clear()
+        txtInterestRate.Clear()
+        txtMonthlyPayment.Clear()
+        lblDisplay.Text = ""
+
+    End Sub
+
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+
+        Dim intRetirementAg As Integer = cmbRetirementAge.SelectedItem()
+        Dim intAge As Integer = txtAge.Text
+        Dim intMonthlyPayment As Decimal = txtMonthlyPayment.Text
+        Dim decInterestRate As Decimal = txtInterestRate.Text
+
+        Dim Nper = CalcPaymentPeriod(intRetirementAg, intAge)
+        Dim Rate = MonthlyInterestRate(decInterestRate)
+        Dim PMT = PaymentPerMonth(intMonthlyPayment)
+
+
+        lblDisplay.Text = CalcFutureValue(PMT, Nper, Rate)
+
+    End Sub
+
+    Private Sub lblDisplay_Click(sender As Object, e As EventArgs) Handles lblDisplay.Click
+
+    End Sub
+End Class
